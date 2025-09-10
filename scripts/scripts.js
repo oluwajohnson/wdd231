fd2031e5e96e36255a70955baecebf8c5be53c70
oluwajohnson <ogunleyejohnson77@gmail.com>
2025-09-10 13:06:29 +0100

    week initial

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+document.getElementById("weather").innerHTML = `
+  <p><strong>75°F</strong> Partly Cloudy</p>
+  <p>High: 85° | Low: 52°</p>
+  <p>Humidity: 34%</p>
+  <p>Sunrise: 7:30am</p>
+  <p>Sunset: 9:59pm</p>
+`;
+
+document.getElementById("todayTemp").textContent = "90°F";
+document.getElementById("tomorrowTemp").textContent = "89°F";
+document.getElementById("nextDayTemp").textContent = "68°F";
